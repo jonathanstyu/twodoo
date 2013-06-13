9c6ee88dcac23219e94e353160e2cf886f2617cf
Jonathan Yu <jonathan@jonathanyu.net>
2013-06-13 16:10:03 -0700

    Finished nested forms for projects/items

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,13 @@
 class Project < ActiveRecord::Base
-  attr_accessible :title, :description, :team_id
+  attr_accessible :title, :description, :team_id, :items_attributes
   
   has_many :items, :inverse_of => :project
   belongs_to :team
   
   validates :title, :description, :presence => true 
+  accepts_nested_attributes_for :items, :reject_if => :empty_title 
   
+  def empty_title(attributes)
+    attributes['title'].blank?
+  end
 end
